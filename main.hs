131399e7b8693da36fcb1a99a1e8f24b1cd7ffdb
Leo <leoferenzini@gmail.com>
2022-01-07 10:59:04 -0300

    Updates printBoard and printWhoIsPlaying functions

--- main.hs
+++ main.hs
@@ -1,13 +1,17 @@
 --Nim game
 
+import Control.Monad
+
 board :: [Int]
 board = [1, 3, 5, 7]
 
-isPlayerPlaying :: Bool -> IO ()
-isPlayerPlaying p = do
+printWhoIsPlaying :: Bool -> IO ()
+printWhoIsPlaying p = do
   if p
-    then putStrLn "Human Player is playing"
-    else putStrLn "Computer is playing"
+    then
+      putStrLn "Human Player is playing"
+    else
+      putStrLn "Computer is playing"
 
 changeTurn :: t0 -> Bool -> Bool
 changeTurn t = return not t
@@ -24,10 +28,9 @@ selectDifficulty = do
   if d == 0
     then return 99
     else return d
-  --return (read difficulty :: Int)
 
-gameMenu :: IO ()
-gameMenu = do
+{-gameMenu :: IO ()
+gameMenu = do --TODO fix the menu
   putStrLn "Welcome to Nim!"
   putStrLn "Are you ready?"
   putStrLn "1. Yes, play"
@@ -35,30 +38,29 @@ gameMenu = do
   putStr "> "
   option <- getLine
   if (read option :: Int) == 1
-    then
-      --let level = selectDifficulty -- TODO: implement difficulty selection
+    then --let level = selectDifficulty -- TODO: implement difficulty selection
+
       let level = 1
        in if level == 1
-            then isPlayerPlaying True
+            then isPlayerPlaying True --TODO fix this
             else
               if level == 2
                 then isPlayerPlaying False
                 else putStrLn "Invalid option"
-    else putStrLn "Bye!"
+    else putStrLn "Bye!"-}
 
 printBoard :: [Int] -> IO ()
 printBoard b = do
-  if (not (null b)) then do
-      let line = "|" ++ (show (head b)) ++ "|"
-      putStrLn line
-      printBoard (tail b)
-    else return ()
+  Control.Monad.when (not (null b)) $ do --TODO print the complete line
+    let line = "|" ++ (show (head b)) ++ "|"
+    putStrLn line
+    printBoard (tail b)
 
 gameLoop :: [Int] -> Bool -> IO ()
 gameLoop board player = do
-  --putStrLn "Board:" -- TODO: print board in the beginning of the game
-  --printBoard board
-  isPlayerPlaying player
+  putStrLn "Board:"
+  printBoard board
+  printWhoIsPlaying player
   putStrLn "Enter the line number"
   putStr "> "
   number <- getLine
@@ -76,11 +78,8 @@ gameLoop board player = do
         then do
           putStrLn "Board:"
           printBoard newBoard
-          putStrLn "You win!"
-        else --return ()
-        do
-          putStrLn "Board:"
-          printBoard newBoard
+          if player then putStrLn "You win!" else putStrLn "Computer wins!"
+        else do
           gameLoop newBoard (not player)
     else do
       putStrLn "Invalid number"
